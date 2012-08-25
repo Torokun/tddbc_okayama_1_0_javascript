9799300d9c44ae26b8810b04dd4075e5fe4b47bc
twingo.b <twingo.b@gmail.com>
2012-08-25 12:41:07 +0900

    総計は0円 １０円を入れると、総計が１０円になる

---
+++ spec/vending_machine_spec.js
@@ -0,0 +1,24 @@
+// 最初はブレストする。それぞれのテストは依存させない。シナリオは考えなくて良い//
+// 総計は０円
+// １０円を入れると、総計が１０円になる
+// １００円を入れると、総計が１００円になる
+// １０円と１００円を入れると、総計が１１０円になる
+// １０円を入れて、払い戻しを行うと、つり銭として、１０円が出力される。
+// １０円をいれて、払い戻しを行うと、総計が０円になる
+
+var createVendingMachine = require('../lib/vending_machine').createVendingMachine;
+
+describe('硬貨を投入していない場合', function() {
+	it('総計は０円', function() {
+		var vm = createVendingMachine();
+		expect(vm.getTotalAmount()).toEqual(0);	
+	});
+});
+
+describe('硬化を投入する場合', function() {
+	it('１０円を入れると、総計が１０円になる', function() {
+		var vm = createVendingMachine();
+		vm.insertMoney(10);
+		expect(vm.getTotalAmount()).toEqual(10);	
+	});
+});
